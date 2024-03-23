2f0e77dd88445622cb8c5e82dc4ea2c7ad60aa41
lesuuda <lesuudaljamani@gmail.com>
2024-03-23 13:44:52 +0300

    install flask with puppet lint usingpip3

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+# using puppet to install flask
+package { 'Flask':
+        ensure   => '2.1.0',
+        provider => 'pip3',
+}
+package { 'Werkzeug':
+        ensure   => '2.1.1',
+        provider => 'pip3',
+}
