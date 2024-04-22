a339bc32429529bdb0e5df020308f064b840b93f
lesuuda <lesuudaljamani@gmail.com>
2024-04-22 16:04:27 +0300

    fixed order of libaries and number of tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,8 +2,8 @@
 """uses a REST API to return employee's TODO List"""
 
 
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks ({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
