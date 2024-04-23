8447ba3e8cd1e3167aee5223d91c6bd4376aae99
lesuuda <lesuudaljamani@gmail.com>
2024-04-23 04:44:23 +0300

    core: export as CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,9 @@
-##!/usr/bin/python3
+#!/usr/bin/python3
 """Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
+
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
