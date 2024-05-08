a227749e104940d52f1e2a26b3b6d4a18ae84419
lesuuda <lesuudaljamani@gmail.com>
2024-05-08 04:22:06 +0300

    added function to return top 10 posts in subredit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""returns the top 10 posts from a sub redit"""
+
+import json  # Add missing import statement
+
+import requests
+
+
+def top_ten(subreddit):
+    """returns the number of subscribers in a subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "lesuudaAPI"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print ("None")
+        return
+    results = response.json().get("data")
+    children = results.get("children")
+    if children is not None:
+        [print(c.get("data").get("title")) for c in children]
